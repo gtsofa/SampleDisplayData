e3c49b78b63843671485be08fad2854bb3dfc257
gtsofa <jlsnyule@yahoo.com>
2020-08-02 19:41:54 +0300

    Initial Commit

---
+++ SampleDisplayData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleDisplayData
+//
+//  Created by Julius on 02/08/2020.
+//  Copyright © 2020 Julius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
